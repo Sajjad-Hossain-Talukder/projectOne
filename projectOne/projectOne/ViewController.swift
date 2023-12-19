9c8a54d116cb6d119a6f41376e8b74105e9b5e1a
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-19 19:17:40 +0600

    Design Completed

--- projectOne/projectOne/ViewController.swift
+++ projectOne/projectOne/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topBanner: UIView!
+    
+    @IBOutlet weak var screenRecordingSpacing: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        screenRecordingSpacing.addCharacterSpacing(kernValue: 0.1)
+    
+
     }
+    
+  
 
 
+}
+
+
+extension UILabel {
+    
+    // adding space between each characters
+    func addCharacterSpacing(kernValue: Double) {
+        if let labelText = text, labelText.isEmpty == false {
+            let attributedString = NSMutableAttributedString(string: labelText)
+            attributedString.addAttribute(.kern,
+                                          value: kernValue,
+                                          range: NSRange(location: 0, length: attributedString.length - 1))
+            attributedText = attributedString
+        }
+    }
 }
 
